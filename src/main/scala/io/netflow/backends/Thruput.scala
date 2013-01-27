e17f137316a993fc3e019f85fb40019c1d349ce8
Franz Bettag <franz@bett.ag>
2013-01-27 21:47:47 +0100

    Updated to wasted-util 0.4 high performance Thruput client (WebSockets).

--- src/main/scala/io/netflow/backends/Thruput.scala
+++ src/main/scala/io/netflow/backends/Thruput.scala
@@ -4,8 +4,10 @@ import io.netflow.flows._
 import io.wasted.util._
 import io.wasted.util.http._
 
+import io.netty.channel.Channel
 import java.net.{ InetSocketAddress, InetAddress }
 import java.util.UUID
+import java.util.concurrent.ConcurrentHashMap
 
 case class ThruputRecipient(platform: ThruputPlatform, toUser: Option[String] = None) {
   def auth = platform.auth
@@ -20,7 +22,7 @@ case class ThruputPlatform(urlStr: String, authStr: String, signStr: String) {
   def msg(fd: Flow[_], addr: InetAddress, toUser: List[String]): String = {
     val user = toUser.length match {
       case 0 => ""
-      case _ => """, "to": ["""" + toUser.mkString("""", """") + """"]"""
+      case _ => """, "to": { "user": ["""" + toUser.mkString("""", """") + """"] }"""
     }
     val ip = """, "ip": """" + addr.getHostAddress + """""""
 
@@ -28,19 +30,37 @@ case class ThruputPlatform(urlStr: String, authStr: String, signStr: String) {
   }
 }
 
-trait ThruputSender {
-  protected val backend: Storage
-  protected var thruputPrefixes: List[InetPrefix]
+object ThruputClientManager {
+  private val thruputClients = new ConcurrentHashMap[ThruputPlatform, Thruput]()
 
-  protected val thruputHttpClient = HttpClient(2)
+  def get(tp: ThruputPlatform) = Option(thruputClients.get(tp)) match {
+    case Some(tp) => Some(tp)
+    case None =>
+      val thruputClient = new Thruput(tp.url.toURI, tp.auth, tp.sign)
+      thruputClient.connect
+      thruputClients.put(tp, thruputClient)
+      Some(thruputClient)
+  }
+
+  def shutdown(tp: ThruputPlatform): Unit = Option(thruputClients.get(tp)).map(_.shutdown)
+}
+
+trait ThruputSender {
+  protected def backend: Storage
+  protected def thruputPrefixes: List[InetPrefix]
 
   protected val thruput = (sender: InetSocketAddress, flow: Flow[_], prefix: InetPrefix, addr: InetAddress) => {
     val iter = backend.getThruputRecipients(sender, prefix).groupBy(_.platform).iterator
     while (iter.hasNext) {
       val platformRcpts = iter.next()
-      val rcpt = platformRcpts._1
-      Tryo(thruputHttpClient.thruput(rcpt.url, rcpt.auth, rcpt.sign, rcpt.msg(flow, addr, platformRcpts._2.flatMap(_.toUser))))
+      val rcptPlatform = platformRcpts._1
+      ThruputClientManager.get(rcptPlatform) match {
+        case Some(tc) =>
+          val msg = rcptPlatform.msg(flow, addr, platformRcpts._2.flatMap(_.toUser))
+          tc.write(msg)
+        case _ =>
+      }
     }
   }
-
 }
+
