7492d8078c68c89697d392cc52adeee1387d3fa3
Franz Bettag <franz@bett.ag>
2013-06-12 01:55:55 +0200

    Added specific IPv6 listeners capabilities.

--- src/main/scala/io/netflow/Server.scala
+++ src/main/scala/io/netflow/Server.scala
@@ -19,8 +19,16 @@ object Server extends App with Logger { PS =>
 
     val eventLoop = new NioEventLoopGroup
     def startListeningFor(what: String, config: String, default: List[String], handler: ChannelHandler): Boolean = {
-      val conf = Config.getStringList(config, default).map(_.split(":"))
-      val listeners = conf.map(l => new java.net.InetSocketAddress(l.head, l.last.toInt))
+      val listeners = Config.getStringList(config, default).flatMap {
+        case ipv4: String if ipv4.matches("""\d+\.\d+\.\d+\.\d+:\d+""") =>
+          val split = ipv4.split(":")
+          Tryo(new java.net.InetSocketAddress(split(0), split(1).toInt))
+        case ipv6: String if ipv6.matches("""\[[0-9a-fA-F:]+\]:\d+""") =>
+          val split = ipv6.split("]:")
+          val addr = split(0).replaceFirst("\\[", "")
+          Tryo(new java.net.InetSocketAddress(java.net.InetAddress.getByName(addr), split(1).toInt))
+        case _ => None
+      }
 
       // Refresh filters from Backend
       Try {
